f25f33d8acd41c68826479fb4cf5677e6551bdbb
Mykyta <98192153+pOpovich69@users.noreply.github.com>
2023-02-13 15:15:12 +0200

    done

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,15 +1,18 @@
 package domain;
 
 public class Exercise {
-    public static int[] Calculate(int quantityElement) {
-        int[] arr = new int[quantityElement];
-        arr[arr.length/2] = 0;
+    //Метод який повертає оброблений масив
+    public static int[] Calculate(int quantityElements) {
+        int[] arr = new int[quantityElements];
+        arr[arr.length/2] = 0; //Елемент по середені дорівнює 0
         int j = 0;
+        //Пробігає по першій половині масиву
         for(int i = (arr.length / 2); i >= 0; i--){
             arr[i] = j;
             j++;
         }
         j = 0;
+        //Пробігає другу половину масиву
         for(int i = (arr.length / 2); i < arr.length; i++){
             arr[i] = j;
             j++;
